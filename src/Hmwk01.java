61f279cfaebd72375a9e508b72fc6fa98f2559c0
Riley Flint <riley.scott.flint@gmail.com>
2017-08-29 22:46:32 -0500

    Changed code from using ArrayList to using arrays and made access to local methods private. Updated Author

--- src/Hmwk01.java
+++ src/Hmwk01.java
@@ -8,28 +8,27 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Hmwk01 {
     public static void main(String[] args) {
         File input = new File("input.txt");
 
-        List<Integer> ints = getInts(input);
+        int[] ints = getInts(input);
 
         printResults(ints);
     }
 
     /**
-     * Returns a list of integers found in the file supplied.
+     * Returns an array of integers found in the file supplied.
      *
      * @param file File you would like to read integers from.
-     * @return Returns an ArrayList of integers found in the inputted file.
+     * @return Returns an array of integers found in the inputted file.
      */
-    static List<Integer> getInts(File file) {
+    private static int[] getInts(File file) {
         Scanner scanner = null;
-        List<Integer> result = new ArrayList<>();
+        //Uses the countIntegers method to find out how large the array needs to be.
+        int[] result = new int[countIntegers(file)];
 
         //Surround with try/catch to handle FileNotFoundException
         //if the file is missing or inaccessible for any reason.
@@ -45,8 +44,10 @@ public class Hmwk01 {
 
         //If the scanner finds an int, read ints until it can't find another.
         if (scanner.hasNextInt()) {
+            int count = 0;
+            //For as long as there is a next int in the file, add it to the result array.
             while (scanner.hasNextInt()) {
-                result.add(scanner.nextInt());
+                result[count++] = scanner.nextInt();
             }
         }
         //If the scanner does not find an int, it will print out
@@ -56,7 +57,55 @@ public class Hmwk01 {
             System.err.println("File \""+file.getName()+"\" does not contain any integers.");
             System.exit(1);
         }
-        //Returns a list of integers found in the file.
+        //Returns an array of integers found in the file.
+        return result;
+    }
+
+    /**
+     * Counts the number of integers in a file.
+     *
+     * @param file File to count the integers in.
+     * @return Returns the number of integers found in the file.
+     */
+    private static int countIntegers(File file) {
+        int result = 0;
+
+        Scanner scanner = null;
+        //Surround with try/catch to handle FileNotFoundException
+        //if the file is missing or inaccessible for any reason.
+        try {
+            scanner = new Scanner(file);
+        }
+        //If FileNotFoundException is thrown, "File "(Name of File)" does not exist."
+        //is printed out for the user in the err stream and the program is terminated.
+        catch (FileNotFoundException e) {
+            System.err.println("File \"" + file.getName() + "\" does not exist.");
+            System.exit(1);
+        }
+
+
+        //If the scanner finds an int, read ints until it can't find another.
+        if (scanner.hasNextInt()) {
+            int count = 0;
+            //The count variable is incremented for each integer that is found in the file.
+            while (scanner.hasNextInt()) {
+                scanner.nextInt();
+                count++;
+            }
+            result = count;
+        }
+        //If the scanner does not find an int, it will print out
+        //"File "(Name of File)" does not contain any integers." in
+        //the err  and the program is terminated.
+        else {
+            System.err.println("File \""+file.getName()+"\" does not contain any integers.");
+            System.exit(1);
+        }
+        if (result == 0) {
+            System.err.println("File \""+file.getName()+"\" does not contain any integers.");
+            System.exit(1);
+        }
+        //Returns the number of integers found in the file.
         return result;
     }
 
@@ -66,23 +115,23 @@ public class Hmwk01 {
      *
      * @param ints Ints to print the results of.
      */
-    static void printResults(List<Integer> ints) {
-        System.out.println("Integers read: " + ints.size());
+    private static void printResults(int[] ints) {
+        System.out.println("Integers read: " + ints.length);
         System.out.println("Smallest Integer: " + getSmallest(ints));
         System.out.println("Largest Integer: " + getLargest(ints));
         System.out.println("Average: " + getAverage(ints));
     }
 
     /**
-     * Returns the smallest int in the list supplied.
+     * Returns the smallest int in the array supplied.
      *
-     * @param ints List of ints to find the smallest one.
-     * @return Returns the smallest int found in the supplied list.
+     * @param ints Array of ints to find the smallest one.
+     * @return Returns the smallest int found in the supplied array.
      */
-    static int getSmallest(List<Integer> ints) {
-        //Sets the variable smallestInt to the first value in the list.
-        int smallestInt = ints.get(0);
-        //Iterates through the list of ints supplied.
+    private static int getSmallest(int[] ints) {
+        //Sets the variable smallestInt to the first value in the array.
+        int smallestInt = ints[0];
+        //Iterates through the array of ints supplied.
         for (int i : ints) {
             //If the int in the current iteration is smaller than the value of the smallestInt
             //variable, then the smallestInt value is replaced by the variable i in the current iteration.
@@ -90,20 +139,20 @@ public class Hmwk01 {
                 smallestInt = i;
             }
         }
-        //Returns the smallest int found in the supplied list.
+        //Returns the smallest int found in the supplied array.
         return smallestInt;
     }
 
     /**
-     * Returns the largest int in the list supplied.
+     * Returns the largest int in the array supplied.
      *
-     * @param ints List of ints to find the largest one.
-     * @return Returns the largest int found in the supplied list.
+     * @param ints Array of ints to find the largest one.
+     * @return Returns the largest int found in the supplied array.
      */
-    static int getLargest(List<Integer> ints) {
-        //Sets the variable largestInt to the first value in the list.
-        int largestInt = ints.get(0);
-        //Iterates through the list of ints supplied.
+    private static int getLargest(int[] ints) {
+        //Sets the variable largestInt to the first value in the array.
+        int largestInt = ints[0];
+        //Iterates through the array of ints supplied.
         for (int i : ints) {
             //If the variable i in the current iteration is larger than the value of the largestInt
             //variable, then the largestInt value is replaced by the variable i in the current iteration.
@@ -111,22 +160,22 @@ public class Hmwk01 {
                 largestInt = i;
             }
         }
-        //Returns the largest int found in the supplied list.
+        //Returns the largest int found in the supplied array.
         return largestInt;
     }
 
     /**
      * Returns the sum of the supplied ints.
      *
-     * @param ints List of ints to find the sum of.
+     * @param ints Array of ints to find the sum of.
      * @return Returns the sum of the supplied ints.
      */
-    static int getSum(List<Integer> ints) {
+    private static int getSum(int[] ints) {
         //The sum starts at 0.
         int sum = 0;
-        //Iterates through the list of ints supplied.
+        //Iterates through the array of ints supplied.
         for (int i : ints) {
-            //Adds each value in the list of ints to the sum.
+            //Adds each value in the array of ints to the sum.
             sum += i;
         }
         //Returns the sum of the supplied ints.
@@ -134,21 +183,21 @@ public class Hmwk01 {
     }
 
     /**
-     * Returns the average of the list of integers.
+     * Returns the average of the array of integers.
      *
-     * @param ints List of ints to find the average of.
-     * @return Returns the average of the list of integers.
+     * @param ints Array of ints to find the average of.
+     * @return Returns the average of the array of integers.
      */
-    static double getAverage(List<Integer> ints) {
+    private static double getAverage(int[] ints) {
         //Uses the getSum method I created to get the sum of the integers.
         double sum = getSum(ints);
-        //Sets the numbers variable to the amount of numbers in the ints list.
-        double numbers = ints.size();
-        //Figures the average of the supplied list of ints.
+        //Sets the numbers variable to the amount of numbers in the ints array.
+        double numbers = ints.length;
+        //Figures the average of the supplied array of ints.
         double average = sum/numbers;
         //Rounds the average down to two decimal points.
         double round = Math.round(average * 100.0) / 100.0;
-        //Returns the average of the list of integers.
+        //Returns the average of the array of integers.
         return round;
     }
 }
